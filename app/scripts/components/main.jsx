d7709bff4dab134f6cd001acec3060f96fdfff2b
smckeon <mckeon.sean@outlook.com>
2017-02-23 15:29:04 -0500

    working out bugs

--- app/scripts/components/main.jsx
+++ app/scripts/components/main.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var Backbone = require('backbone');
 
 var ImageCollection = require('../models/image.js').ImageCollection;
-
+var Images
 
 
 var ImageBoardContainer = React.createClass({
@@ -11,8 +11,10 @@ var ImageBoardContainer = React.createClass({
 
     console.log(imageCollection);
     return {
-      imageCollection: imageCollection
-    }
+      imageCollection: imageCollection,
+      showForm: false,
+      imageToEdit: new Image()
+    };
   },
   componentWillMount(){
     console.log(this.state.imageCollection);
@@ -24,12 +26,38 @@ var ImageBoardContainer = React.createClass({
     ]);
     this.setState({imageCollection: newImageCollection})
   },
+  handleToggleForm: function(event){
+    event.preventDefault();
+    this.setState({showForm: !this.state.showForm})
+  },
+  addImage: function(image){
+    var imageCollection = this.state.imageCollection;
+    this.state.imageCollection.add(image);
+    this.setState({imageCollection: imageCollection});
+    // ______________^^^^^^^^^^^^^^^
+    // HOOK UP MORE ABOVE AFTER CLASS
+  },
+  editImage: function(imageToEdit){
+    this.setState({showForm: true, imageToEdit: imageToEdit});
+  },
   render: function(){
   return (
-    // this.state.imageCollection  -- can use the state to access image collection
-    <div>
+
+    <div className="container">
+
+      <ul className="nav nav-pills">
+        <li role="presentation" className="active">
+          <a onClick={this.handleToggleForm} href="#">+</a>
+        </li>
+      </ul>
+
+        {this.state.showForm ? <ImageForm imageToEdit={this.state.imageToEdit} addImage={this.addImage} /> : null}
+
       <ImageForm />
-      <ImageList imageCollection={this.state.imageCollection} />
+      <ImageList
+        imageCollection={this.state.imageCollection},
+        editImage={this.editImage}
+        />
     </div>
   );
  }
@@ -37,10 +65,44 @@ var ImageBoardContainer = React.createClass({
 
 
 var ImageForm = React.createClass({
+  propTypes: {
+    addImage: React.PropTypes.func.isRequired,
+  },
+  getInitialState: function(){
+    return {
+      // url: '',
+      // description: ''
+      this.props.imageToEdit.toJSON();
+    }
+  },
+  componentWillReceiveProps: function(nextProps){
+    // console.log(nextProps.imageToEdit);
+    this.setState(nextProps.imageToEdit.toJSON());
+  },
+  handleUrlChange: function(){
+    this.setState({'url': event.target.value});
+  },
+  handleDescriptionChange: function(){
+    this.setState({'description': event.target.value});
+  },
+  handleSubmit: function(event){
+    event.preventDefault();
+    // console.log(this.state);
+    this.props.addImage(this.state);
+    this.setState({url: '', description: ''});
+  },
   render: function(){
     return(
-      <form>
-        // .. STUFF HERE PLZ
+      <form className="well">
+        <div className="form-group">
+          <label htmlFor="url">Image URL</label>
+          <input onChange={this.handleUrlChange} value={this.state.url} type="text" className="form-control" id="url" placeholder="http://" />
+        </div>
+        <div className"form-group">
+          <label htmlFor="description">Description</label>
+          <input onClick={this.handleDescriptionChange} value={this.state.description} type="text" className="form-control" id="description" placeholder="Password" />
+        </div>
+        <button type="submit" className="btn btn-success">Add Image</button>
       </form>
     );
   }
@@ -49,12 +111,14 @@ var ImageForm = React.createClass({
 
 var ImageList = React.createClass({
   propTypes: {
-    imageCollection: React.PropTypes.instanceOf(Backbone.Collection).isRequired
+    imageCollection: React.PropTypes.instanceOf(Backbone.Collection).isRequired,
+    editImage: React.PropTypes.func.isRequired
   },
 
   render: function (){
     console.log(this.props.imageCollection);
 
+    var self = this;
     var imageBoardList = this.props.imageCollection.map(function(image){
       return (
 
@@ -63,7 +127,14 @@ var ImageList = React.createClass({
           <div className="caption">
             {/* <h3>Thumbnail label</h3> */}
             <p>{image.get('description')}</p>
-            <p><a href="#" className="btn btn-primary" role="button">Button</a> <a href="#" className="btn btn-default" role="button">Button</a></p>
+            <p>
+              <a
+                onClick={function(e){
+                  e.preventDefault();
+                  self.props.editImage(image);
+                }} href="#" className="btn btn-warning" role="button">Edit</a>
+              <a href="#" className="btn btn-danger" role="button">Delete</a>
+            </p>
           </div>
         </div>
       )
